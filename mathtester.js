a55cd40925a07c07a8dfce47110e34965d50296e
53142 <89957019+53142@users.noreply.github.com>
2024-07-28 20:41:36 -0700

    asdf

--- mathtester.js
+++ mathtester.js
@@ -3,66 +3,106 @@ Input = document.getElementById("inputbox");
 Score = document.getElementById("score");
 Timer = document.getElementById("timer");
 Start = document.getElementById("start");
+DifficultySelector = document.getElementById("difficultyselector");
 
-const operations = ["+", "-", "*", "/"];
-let QuestionAnswer = null;
+const modes = ["easy", "medium", "hard"];
+
+const simple_operations = ["+", "-", "*", "/"];
 let gameStarted = false;
+
+let question = null;
+let answer = null;
 
 let sec = 30;
 let timerID = null;
+let correctanswer = null;
 
-function getQuestionAnswer() {
-    let operation = operations[Math.floor(Math.random() * operations.length)];
-    let num1 = Math.floor(Math.random() * 13);
+// Event listeners
+Start.addEventListener("click", function() {
+    // If game is not already started
+    if (!gameStarted) {
+        gameStarted = true;
+        score.innerHTML = "0";
+        Input.value = "";
+
+        // Start timer
+        sec = 30;
+        timerID = setInterval(() =>{
+            Timer.innerHTML=sec;
+            sec--;
+            if (sec < 0) {
+                timeUp();
+                return;
+            }
+        }, 1000);
+
+        let QuestionAnswer = getQuestion(DifficultySelector.selectedIndex);
+        // Set question/answer variables
+        question = QuestionAnswer[0];
+        answer = QuestionAnswer[1];
+        
+        console.log(QuestionAnswer);
+
+        // Display question
+        Question.innerHTML = question;
+    }
+})
+
+Input.addEventListener("keyup", function(event) {
+    if (gameStarted && (Input.value == answer)) {
+        score.innerHTML = parseInt(score.innerHTML) + 1;
+        Input.value = "";
+        correctanswer = true;
+    }
+})
+
+
+// Game Functions
+function getQuestion(mode) {
+    let answer = null;
+    let num1 = null;
     let num2 = null;
+    let operation = null;
 
-    if (operation == "/") {
-        // Prevent division by zero
-        num2 = Math.floor(Math.random() * 12) + 1;
-    } else {
-        num2 = Math.floor(Math.random() * 13);
+    switch (mode) {
+        case 0: // Easy
+            // Choose random operation
+            operation = simple_operations[Math.floor(Math.random() * simple_operations.length)];
+
+            // Choose random number
+            num1 = Math.floor(Math.random() * 13);
+
+            // If division, prevent division by zero and decimal points
+            if (operation == "/") {
+                do {
+                    num2 = Math.floor(Math.random() * 12) + 1;
+                } while (num1 % num2 != 0);
+            } else {
+                num2 = Math.floor(Math.random() * 13);
+            }
+            // Get answer
+            switch(operation) {
+                case "+":
+                    answer = num1 + num2;
+                    break;
+                case "-":
+                    answer = num1 - num2;
+                    break;
+                case "*":
+                    answer = num1 * num2;
+                    break;
+                case "/":
+                    answer = num1 / num2;
+                    break;
+            }
+            break;
+        case 1: // Medium
+            break;
+
+        case 2: // Hard
+            break;
     }
-    let answer = getAnswer(num1, operation, num2);
-    console.log("Answer: " + answer);
     return [`What is ${num1} ${operation} ${num2}?`, answer];
-}
-
-function getAnswer(num1, operation, num2) {
-    switch (operation) {
-        case "+":
-            return num1 + num2;
-        case "-":
-            return num1 - num2;
-        case "*":
-            return num1 * num2;
-        case "/":
-            return num1 / num2;
-    }
-}
-
-function makeQuestion() {
-    QuestionAnswer = getQuestionAnswer();
-    
-    // If there is a decimal point, discard question and make a new one
-    if (QuestionAnswer[1] % 1 != 0) {
-        makeQuestion();
-    }
-
-    Question.innerHTML = QuestionAnswer[0];
-}
-
-function startTimer() {
-    sec = 30;
-    timerID = setInterval(timer, 1000);
-}
-
-function timer() {
-    Timer.innerHTML=sec;
-        sec--;
-        if (sec < 0) {
-            timeUp();
-            return;
-        }
 }
 
 function timeUp() {
@@ -78,22 +118,3 @@ function timeUp() {
     }
     return;
 }
-
-Input.addEventListener("keyup", function(event) {
-    if (gameStarted && (Input.value == QuestionAnswer[1])) {
-        score.innerHTML = parseInt(score.innerHTML) + 1;
-        makeQuestion();
-        Input.value = "";
-    }
-})
-
-Start.addEventListener("click", function() { 
-    if (gameStarted) {
-        return;
-    }
-    gameStarted = true;
-    score.innerHTML = "0";
-    Input.value = "";
-    makeQuestion();
-    startTimer();
-})
