31e321b4eea8d0a537ece5a6b53c03534f0c5816
53142 <89957019+53142@users.noreply.github.com>
2024-08-03 20:15:16 -0700

    asdf

--- scripts/matrixcalculator.js
+++ scripts/matrixcalculator.js
@@ -6,10 +6,15 @@ let columnsB = document.getElementById('columnsValueB');
 
 let matrixA = document.getElementById('matrixA');
 let matrixB = document.getElementById('matrixB');
+let resultMatrix = document.getElementById('resultMatrix');
+
+let determinantButtonA = document.getElementById('determinantButtonA');
+let inverseButtonA = document.getElementById('inverseButtonA');
+let determinantButtonB = document.getElementById('determinantButtonB');
+let inverseButtonB = document.getElementById('inverseButtonB');
 
 let addButton = document.getElementById('addButton');
 let subtractButton = document.getElementById('subtractButton');
-
 
 let matrixAValue = [];
 
@@ -68,6 +73,18 @@ columnsB.addEventListener('change', function() {
     updateRowsColumns(matrixB, rowsB.value, columnsB.value);
 });
 
+determinantButtonA.addEventListener('click', function() {
+    updateMatrixAValue();
+    let determinant;
+    
+    if (rowsA.value == 2 && columnsA.value == 2) {
+        determinant = matrixAValue[0][0] * matrixAValue[1][1] - matrixAValue[0][1] * matrixAValue[1][0];
+    } else {
+        alert('Determinant only works for 2x2 matrices');
+        return;
+    }
+    alert('Determinant: ' + determinant);
+});
 
 
 addButton.addEventListener('click', function() {
@@ -84,7 +101,7 @@ addButton.addEventListener('click', function() {
 
     let rowsC = rowsA.value;
     let columnsC = columnsA.value;
-    matrixC.innerHTML = '';
+    resultMatrix.innerHTML = '';
     for (let i = 0; i < rowsC; i++) {
         let row = document.createElement('tr');
         for (let j = 0; j < columnsC; j++) {
@@ -96,15 +113,13 @@ addButton.addEventListener('click', function() {
             column.appendChild(input);
             row.appendChild(column);
         }
-        matrixC.appendChild(row);
+        resultMatrix.appendChild(row);
     }
 });
 
 subtractButton.addEventListener('click', function() {
     updateMatrixAValue();
     updateMatrixBValue();
-    
-    let matrixC = document.getElementById('matrixC');
 
     // Make sure matrices have the same dimensions
     if (rowsA.value != rowsB.value || columnsA.value != columnsB.value) {
@@ -114,7 +129,7 @@ subtractButton.addEventListener('click', function() {
 
     let rowsC = rowsA.value;
     let columnsC = columnsA.value;
-    matrixC.innerHTML = '';
+    resultMatrix.innerHTML = '';
     for (let i = 0; i < rowsC; i++) {
         let row = document.createElement('tr');
         for (let j = 0; j < columnsC; j++) {
@@ -126,6 +141,37 @@ subtractButton.addEventListener('click', function() {
             column.appendChild(input);
             row.appendChild(column);
         }
-        matrixC.appendChild(row);
+        resultMatrix.appendChild(row);
+    }
+});
+
+multiplyButton.addEventListener('click', function() {
+    updateMatrixAValue();
+    updateMatrixBValue();
+
+    // Make sure matrices have the same dimensions
+    if (columnsA.value != rowsB.value) {
+        alert('Matrix A columns must be equal to Matrix B rows');
+        return;
+    }
+
+    let rowsC = rowsA.value;
+    let columnsC = columnsB.value;
+    resultMatrix.innerHTML = '';
+    for (let i = 0; i < rowsC; i++) {
+        let row = document.createElement('tr');
+        for (let j = 0; j < columnsC; j++) {
+            let column = document.createElement('td');
+            let input = document.createElement('input');
+            input.setAttribute('type', 'number');
+            let value = 0;
+            for (let k = 0; k < columnsA.value; k++) {
+                value += matrixAValue[i][k] * matrixBValue[k][j];
+            }
+            input.setAttribute('value', value);
+            column.appendChild(input);
+            row.appendChild(column);
+        }
+        resultMatrix.appendChild(row);
     }
 });
