a3503cbc06fd8d6e181326e3b3b80ffa3225a6ba
53142 <89957019+53142@users.noreply.github.com>
2024-12-18 16:52:50 -0700

    asdf

--- scripts/unicodecopier.js
+++ scripts/unicodecopier.js
@@ -4,6 +4,8 @@ let copyButton = document.getElementById("copyButton");
 
 let outputUnicodeVisible = false;
 
+let coolMenu = document.getElementById("coolMenu");
+
 input.addEventListener("input", function() {
     if (input.value == "") {
         output.innerHTML = "Enter a Unicode code point to display here. For example: 42";
@@ -29,4 +31,18 @@ copyButton.addEventListener("click", function() {
 
 document.getElementById("backButton").addEventListener('click', () => {
     window.location.href = "index.html";
+});
+
+document.getElementById("coolMenuButton").addEventListener('click', () => {
+    if (coolMenu.style.display == "none" || coolMenu.style.display == "") {
+        coolMenu.style.display = "block";
+    } else {
+        coolMenu.style.display = "none";
+    }
+});
+
+[...document.getElementsByClassName("coolMenuCopy")].forEach(element => {
+    element.addEventListener('click', () => {
+        navigator.clipboard.writeText(String.fromCodePoint(element.dataset.unicode));
+    });
 });
